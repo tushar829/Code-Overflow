4baa184d9fe78a52fbc1de6ed9722d7bb7ae7276
harshahvg7 <72880742+harshahvg7@users.noreply.github.com>
2020-10-18 15:22:44 +0530

    Length.md
    Optimised code of length of string in a quick way

---
+++ Length of string.py
@@ -0,0 +1,10 @@
+#Python program to demonstrate the use of 
+# len() method   
+  
+# Length of below string is 5 
+string = "geeks" 
+print(len(string)) 
+  
+# Length of below string is 15 
+string = "geeks for geeks" 
+print(len(string)) 
